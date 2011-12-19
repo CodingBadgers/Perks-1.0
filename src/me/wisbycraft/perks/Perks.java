f4ce141ae307ef3b7c77ce98261f5c452821bc94
TheFish97 <TheFish97@2df59c3f-94b9-4873-9e60-c579250e45c4>
2011-12-19 00:02:05 +0000

    added enable and disable text.
    added logging code to the main class
    
    i would do more but it is too late.
    
    git-svn-id: https://subversion.assembla.com/svn/wisbycraft-perks@12 2df59c3f-94b9-4873-9e60-c579250e45c4

--- src/me/wisbycraft/perks/Perks.java
+++ src/me/wisbycraft/perks/Perks.java
@@ -1,9 +1,13 @@
 package me.wisbycraft.perks;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.bukkit.Server;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
+import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -11,15 +15,38 @@ public class Perks extends JavaPlugin {
 	
 	private final PerksPlayerListener playerListener = new PerksPlayerListener(this);
 	private final PerksInputListener inputListener = new PerksInputListener(this, playerListener);
+        
+        // loging stuff
+        private static final Logger log = Logger.getLogger("minecraft");
+        public String logprefix = "[Perks]";
+        
+        // enable/disable variables
+        public String name;
+        public String version;
+        public Server server;
 
 	@Override
 	public void onDisable() {
-		
+                // Disable Variables
+		server = getServer();
+                PluginDescriptionFile pdf = this.getDescription();
+                
+                name = pdf.getName();
+                version = pdf.getVersion();
+                
+                // Disable Text
+                log.log(Level.INFO, logprefix + " " + name + " v: " + version + " has been disabled successfuly.");
 	}
 
 	@Override
 	public void onEnable() {
-		PluginManager pm = this.getServer().getPluginManager();
+                // enable variables
+                server = getServer();
+		PluginManager pm = server.getPluginManager();
+                PluginDescriptionFile pdf = this.getDescription();
+                
+                name = pdf.getName();
+                version = pdf.getVersion();
 		
 		// Player listeners
 		pm.registerEvent(Event.Type.PLAYER_JOIN, playerListener, Event.Priority.Normal, this);
@@ -29,6 +56,9 @@ public class Perks extends JavaPlugin {
 		
 		// Input Listener
 		pm.registerEvent(Event.Type.CUSTOM_EVENT, inputListener, Event.Priority.Normal, this);
+                
+                // Enable Text
+                log.log(Level.INFO, logprefix + " " + name + " v: " + version + " has been enabled sucessfuly.");
 	}
 	
 	@Override
