ba3039add3eb45e0f8d3c7764b756f8521e10c9a
TheFish97 <TheFish97@2df59c3f-94b9-4873-9e60-c579250e45c4>
2011-12-20 08:32:11 +0000

    changed from println to log.
    git-svn-id: https://subversion.assembla.com/svn/wisbycraft-perks@24 2df59c3f-94b9-4873-9e60-c579250e45c4

--- src/me/wisbycraft/perks/PerkUtils.java
+++ src/me/wisbycraft/perks/PerkUtils.java
@@ -1,11 +1,16 @@
 package me.wisbycraft.perks;
 
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.bukkit.entity.Player;
 
 public class PerkUtils {
+    
+    private static final Logger log = Logger.getLogger("minecraft");
 
         static public void DebugConsole(String messsage) {
-                System.out.println("[Perks] " + messsage + ".");
+                log.log(Level.INFO, "[Perks] " + messsage + ".");
         }
 	static public PerkPlayerArray perkPlayers = new PerkPlayerArray();
 	
