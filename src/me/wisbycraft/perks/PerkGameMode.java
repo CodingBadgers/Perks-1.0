d03e88b3a09f3d31d330f257bd2d942b5bbc4aec
TheFish97 <TheFish97@2df59c3f-94b9-4873-9e60-c579250e45c4>
2011-12-29 15:41:53 +0000

    Added in gamemode checking - soon to be for other players
    added in survival and creative cmds
    
    git-svn-id: https://subversion.assembla.com/svn/wisbycraft-perks@35 2df59c3f-94b9-4873-9e60-c579250e45c4

--- src/me/wisbycraft/perks/PerkGameMode.java
+++ src/me/wisbycraft/perks/PerkGameMode.java
@@ -27,6 +27,37 @@ public class PerkGameMode {
 		}
         }
         
+        // checks the players gamemode 
+        if (cmd.getName().equalsIgnoreCase("gm") || cmd.getName().equalsIgnoreCase("gamemode")) {
+                if (args.length == 0) {
+                    if (player.hasPermission("perks.gamemode.check", true)) {
+                        // outputs the players gamemode
+                        PerkUtils.OutputToPlayer(player, "You are in " + player.getPlayer().getGameMode());
+                    }
+                } else if (args.length == 1) {
+                    if (player.hasPermission("perks.gamemode.check.other", true)) {
+                        // this will check another players gamemode.
+                    }
+                }
+            
+        }
+        
+        // change to creative mode
+       if (cmd.getName().equalsIgnoreCase("creative")) {
+           if (player.hasPermission("perks.gamemode.creative", true)) {
+               player.getPlayer().setGameMode(GameMode.CREATIVE);
+               PerkUtils.OutputToPlayer(player, "Now in creative mode");
+           }
+       } 
+       
+       // change to survival mode
+       if (cmd.getName().equalsIgnoreCase("survival")) {
+           if (player.hasPermission("perks.gamemode.survival", true)) {
+               player.getPlayer().setGameMode(GameMode.SURVIVAL);
+               PerkUtils.OutputToPlayer(player, "Now in creative mode");
+           }
+       }
+       
         return false;
     }
 }
