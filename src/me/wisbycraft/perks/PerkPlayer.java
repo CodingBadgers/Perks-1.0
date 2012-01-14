9401c378d07e4f431d691b56715cafd7e06fb411
n3wton <n3wton@2df59c3f-94b9-4873-9e60-c579250e45c4>
2012-01-14 20:04:57 +0000

    First Tidy up...
    Moved vars into private classes.
    
    Next add some easy to use methods to reduce duplicate code...
    
    But now its tea time :D
    
    git-svn-id: https://subversion.assembla.com/svn/wisbycraft-perks@42 2df59c3f-94b9-4873-9e60-c579250e45c4

--- src/me/wisbycraft/perks/PerkPlayer.java
+++ src/me/wisbycraft/perks/PerkPlayer.java
@@ -15,44 +15,48 @@ public class PerkPlayer {
 	private Player m_player = null;					//!< store the bukkit player
 	private SpoutPlayer m_spoutPlayer = null;		//!< store the spout player (spout has more commands so if we can use them why not :P)
 
-	// Fly related members //
-	private boolean m_flying = false;				//!< is the player flying?
-	private PerkMagicCarpet m_magicCarpet = null;	//!< A players magic carpet object (Non spout only)
-	private boolean m_forceCarpet = false;			//!< Force magic carpet even when using spout
-	// end fly related members //
+	private class Flying {
+		public boolean m_flying = false;				//!< is the player flying?
+		public PerkMagicCarpet m_magicCarpet = null;	//!< A players magic carpet object (Non spout only)
+		public boolean m_forceCarpet = false;			//!< Force magic carpet even when using spout
+	}
 	
-	// hunger related members //
-	private float m_hungerRate = 0.25f;				//!< means hunger goes down at 1/4 the normal rate <- will load from config when thats done
-	private float m_hungerCounter = 0.0f;			//!< stores the last hunger counter, if this is equal to 1.0 (100%) let a hunger event work as normal
-	// end hunger related members //
+	private class Hunger {
+		public float m_hungerRate = 0.25f;				//!< means hunger goes down at 1/4 the normal rate <- will load from config when thats done
+		public float m_hungerCounter = 0.0f;			//!< stores the last hunger counter, if this is equal to 1.0 (100%) let a hunger event work as normal
+	}
 	
-	// tp related members //
-	private PerkPlayerArray tpRequest = new PerkPlayerArray();
-	private ArrayList<Long> tpRequestTime = new ArrayList<Long>();
-	// end tp related members //
+	private class TP {
+		public PerkPlayerArray m_tpRequest = new PerkPlayerArray();
+		public ArrayList<Long> m_tpRequestTime = new ArrayList<Long>();
+	}
+	
+	private Flying m_fly = null;
+	private Hunger m_hunger = null;
+	private TP m_tp = null;
 	
 	public PerkPlayer(Player player) {
 		m_player = player;
 		
 		if (PerkUtils.spoutEnabled) {
 			m_spoutPlayer = (SpoutPlayer) player;
-	
-			// if the player isnt using spout make a magic carpet
-			if (!m_spoutPlayer.isSpoutCraftEnabled()) {
-				m_magicCarpet = new PerkMagicCarpet();
-			}
 		}
-		else
-		{
-			m_magicCarpet = new PerkMagicCarpet();
+		
+		m_fly = new Flying();
+		m_hunger = new Hunger();
+		m_tp = new TP();
+
+		// if the player isnt using spout make a magic carpet
+		if (!PerkUtils.spoutEnabled || !m_spoutPlayer.isSpoutCraftEnabled()) {
+			m_fly.m_magicCarpet = new PerkMagicCarpet();
 		}
 	}
-
+	
 	// called when a player is kicked or leaves...
 	// all cleanups should be done in here
 	public void remove() {
-		if (m_magicCarpet != null) {
-			m_magicCarpet.destroy();
+		if (m_fly.m_magicCarpet != null) {
+			m_fly.m_magicCarpet.destroy();
 		}
 	}
 
@@ -78,31 +82,31 @@ public class PerkPlayer {
 		// if player isnt using spout create or destroy there magic carpet
 		if (!PerkUtils.spoutEnabled || !m_spoutPlayer.isSpoutCraftEnabled() || forceCarpet) {
 			if (flying) {
-				m_magicCarpet.create(m_player);
+				m_fly.m_magicCarpet.create(m_player);
 			} else {
-				m_magicCarpet.destroy();
+				m_fly.m_magicCarpet.destroy();
 			}
 		}
 
 		// store whether we're flying or not
-		m_flying = flying;
-		setForceCarpet(forceCarpet);
+		m_fly.m_flying = flying;
+		m_fly.m_forceCarpet = forceCarpet;
 	}
 
 	public boolean isFlying() {
-		return m_flying;
+		return m_fly.m_flying;
 	}
 
 	public boolean getForceCarpet() {
-		return m_forceCarpet;
+		return m_fly.m_forceCarpet;
 	}
 
 	public void setForceCarpet(boolean forceCarpet) {
-		m_forceCarpet = forceCarpet;
+		m_fly.m_forceCarpet = forceCarpet;
 	}
 
 	public PerkMagicCarpet getMagicCarpet() {
-		return m_magicCarpet;
+		return m_fly.m_magicCarpet;
 	}
 
 	// checks whether a player has permission to do something or not
@@ -115,7 +119,6 @@ public class PerkPlayer {
 		if (reportError)
 			PerkUtils.OutputToPlayer(this, "You do not have permission to use that command");
 		
-		
 		return false;
 	}
 
@@ -123,11 +126,11 @@ public class PerkPlayer {
 	public boolean shouldLowerHunger() {
 		
 		// increase the hunger counter
-		m_hungerCounter += m_hungerRate;
+		m_hunger.m_hungerCounter += m_hunger.m_hungerRate;
 		
 		// if the counter is at 100%, reset the counter and let the hunger event fire
-		if (m_hungerCounter == 1.0f) {
-			m_hungerCounter = 0.0f;
+		if (m_hunger.m_hungerCounter == 1.0f) {
+			m_hunger.m_hungerCounter = 0.0f;
 			return true;
 		}
 		
@@ -140,18 +143,18 @@ public class PerkPlayer {
 		boolean newRequest = false;
 		Calendar cal = Calendar.getInstance();
 		
-		if (tpRequest.getPlayer(player.getPlayer()) == null) {
-			tpRequest.add(player);
-			tpRequestTime.add(cal.getTimeInMillis());
+		if (m_tp.m_tpRequest.getPlayer(player.getPlayer()) == null) {
+			m_tp.m_tpRequest.add(player);
+			m_tp.m_tpRequestTime.add(cal.getTimeInMillis());
 			newRequest = true;
 		}
 		
 		// if were requesting to a player we've requested to before check times
 		if (!newRequest) {
-			for (int i = 0; i < tpRequest.size(); ++i) {
-				if (tpRequest.get(i) == player) {
+			for (int i = 0; i < m_tp.m_tpRequest.size(); ++i) {
+				if (m_tp.m_tpRequest.get(i) == player) {
 					// see if 30 seconds have passed, if not tell them to fuck off.
-					Long timeSinceLastReguest = cal.getTimeInMillis() - tpRequestTime.get(i);
+					Long timeSinceLastReguest = cal.getTimeInMillis() - m_tp.m_tpRequestTime.get(i);
 					if(timeSinceLastReguest < 30000) {
 						PerkUtils.OutputToPlayer(player, "Please wait another " + Float.valueOf((new DecimalFormat("##.##").format(30-(timeSinceLastReguest/1000))))  + " seconds before sending another tp request to " + m_player.getName());
 						return;
@@ -159,7 +162,7 @@ public class PerkPlayer {
 					else
 					{
 						// update the time so that they have to wait another 30 seconds.
-						tpRequestTime.set(i, cal.getTimeInMillis());
+						m_tp.m_tpRequestTime.set(i, cal.getTimeInMillis());
 					}
 				}
 			}
@@ -176,15 +179,15 @@ public class PerkPlayer {
 		// if they havn't specified a player, get the last one from the array
 		if (player == null) {
 			
-			if (tpRequest.size() == 0) {
+			if (m_tp.m_tpRequest.size() == 0) {
 				PerkUtils.OutputToPlayer(this, "You have not recieved a tp request from any players");
 				return;
 			}			
 			
-			player = tpRequest.get(tpRequest.size() - 1);
+			player = m_tp.m_tpRequest.get(m_tp.m_tpRequest.size() - 1);
 		}
 		
-		if (tpRequest.getPlayer(player.getPlayer()) == null) {
+		if (m_tp.m_tpRequest.getPlayer(player.getPlayer()) == null) {
 			PerkUtils.OutputToPlayer(this, "You have not recieved a tp request from that player");
 			return;
 		}
@@ -192,7 +195,7 @@ public class PerkPlayer {
 		PerkUtils.OutputToPlayer(player, m_player.getName() + " has accepted your tp request");
 		player.getPlayer().teleport(m_player);
 		
-		tpRequest.removePlayer(player.getPlayer());
+		m_tp.m_tpRequest.removePlayer(player.getPlayer());
 		
 	}
 
@@ -201,22 +204,22 @@ public class PerkPlayer {
 		// if they havn't specified a player, get the last one from the array
 		if (player == null) {
 			
-			if (tpRequest.size() == 0) {
+			if (m_tp.m_tpRequest.size() == 0) {
 				PerkUtils.OutputToPlayer(this, "You have not recieved a tp request from any players");
 				return;
 			}			
 			
-			player = tpRequest.get(tpRequest.size() - 1);
+			player = m_tp.m_tpRequest.get(m_tp.m_tpRequest.size() - 1);
 		}
 		
-		if (tpRequest.getPlayer(player.getPlayer()) == null) {
+		if (m_tp.m_tpRequest.getPlayer(player.getPlayer()) == null) {
 			PerkUtils.OutputToPlayer(this, "You have not recieved a tp request from that player");
 			return;
 		}
 		
 		PerkUtils.OutputToPlayer(player, m_player.getName() + " has declined your tp request");
 		
-		tpRequest.removePlayer(player.getPlayer());
+		m_tp.m_tpRequest.removePlayer(player.getPlayer());
 		
 	}
 }
