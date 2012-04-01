812691345711ebd716ebf40a36534af1ee531efb
n3wton <n3wton@2df59c3f-94b9-4873-9e60-c579250e45c4>
2012-04-01 21:45:14 +0000

    check for water damage before getting the player
    git-svn-id: https://subversion.assembla.com/svn/wisbycraft-perks@103 2df59c3f-94b9-4873-9e60-c579250e45c4

--- src/me/wisbycraft/perks/donator/PerkUnlimitedAir.java
+++ src/me/wisbycraft/perks/donator/PerkUnlimitedAir.java
@@ -12,39 +12,40 @@ import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 public class PerkUnlimitedAir {
 
 	static public void drown(EntityDamageEvent event) {
-
-		if (!(event.getEntity() instanceof Player))
-			return;
-
-		Player bPlayer = (Player) event.getEntity();
 		
-		if (bPlayer == null)
-			return;
-		
-		PerkPlayer player = PerkUtils.getPlayer(bPlayer);
-		
-		if (player == null)
-			return;
-
-		if (!player.hasPermission("perks.water", false))
-			return;
-
 		if (event.getCause() == DamageCause.DROWNING) {
+
+			if (!(event.getEntity() instanceof Player))
+				return;
+	
+			Player bPlayer = (Player) event.getEntity();
+			
+			if (bPlayer == null)
+				return;
+			
+			PerkPlayer player = PerkUtils.getPlayer(bPlayer);
+			
+			if (player == null)
+				return;
+	
+			if (!player.hasPermission("perks.water", false))
+				return;
+			
+			if (!player.hasPermission("perks.water.plus", false))
 			{
-				if (!player.hasPermission("perks.water.plus", false))
-				{
-					if (bPlayer.getInventory().getHelmet().getType() == Material.GOLD_HELMET) {
-						bPlayer.setRemainingAir(bPlayer.getMaximumAir());
-						event.setCancelled(true);
-					}
-				}
-				else
-				{
+				if (bPlayer.getInventory().getHelmet().getType() == Material.GOLD_HELMET) {
 					bPlayer.setRemainingAir(bPlayer.getMaximumAir());
 					event.setCancelled(true);
 				}
 			}
+			else
+			{
+				bPlayer.setRemainingAir(bPlayer.getMaximumAir());
+				event.setCancelled(true);
+			}
+	
 		}
+		
 	}
 
 }
